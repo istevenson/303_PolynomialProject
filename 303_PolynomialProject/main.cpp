ca5e281c69ed59f6845f3b6affeaaa9454cd519e
Imon Stevenson <imon.stevenson@gmail.com>
2016-06-17 17:17:05 -0500

    Initial Commit

---
+++ 303_PolynomialProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  303_PolynomialProject
+//
+//  Created by Imon Stevenson on 6/17/16.
+//  Copyright (c) 2016 Imon Stevenson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
